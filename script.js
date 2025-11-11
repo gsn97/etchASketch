df47c284cea0cf3598c45d77722a347cb8aa7e92
gsn97 <186332589+gsn97@users.noreply.github.com>
2025-11-10 21:14:24 -0500

    Add wrapping ability to canvas container

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const body = document.querySelector(`body`);
 body.style.backgroundColor = `tan`;
+body.style.height = `100vh`;
 body.style.display = `flex`;
+body.style.justifyContent = `center`;
+body.style.alignItems = `center`;
 
-const canvasSizeNum = 500;
+const canvasSizeNum = 750;
 const canvasSize = canvasSizeNum + `px`;
 
 let rowAmount = 16; // grid will be (rowAmount x rowAmount)
@@ -12,21 +15,24 @@ canvas.style.width = canvasSize;
 canvas.style.height = canvasSize;
 canvas.style.border = `solid 2px black`;
 canvas.style.display = `flex`;
+canvas.style.flexWrap = `wrap`;
 body.appendChild(canvas);
 
 function loadCell () {
     const cell = document.createElement(`div`);
+    cell.classList.add(`cell`);
     const cellSize = (canvasSizeNum / rowAmount) + `px`;
     cell.style.width = cellSize;
     cell.style.height = cellSize;
     cell.style.backgroundColor = `white`;
+    cell.style.flexShrink = 0;
     canvas.appendChild(cell);
 };
 
 function loadCanvasGrid () {
     for (i = 1; i <= (rowAmount ** 2); i++) {
         loadCell();
-        canvas.lastChild.id = `cellNum` + i;
+        canvas.lastChild.id = `num` + i;
     };
 };
 
