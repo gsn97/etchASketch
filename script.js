c0ce3823417e75798c76dc52dd5f483fba5ea7c9
gsn97 <186332589+gsn97@users.noreply.github.com>
2025-11-10 19:30:53 -0500

    Create function for loading a single cell on canvas

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const makeDiv = (document.createElement(`div`));
+
 const body = document.querySelector(`body`);
 body.style.backgroundColor = `tan`;
 
 const canvasSizeNum = 500;
 const canvasSize = canvasSizeNum + `px`;
 
-const canvas = document.createElement(`div`);
+let rowAmount = 10;
+
+const canvas = makeDiv;
 canvas.style.width = canvasSize;
 canvas.style.height = canvasSize;
 canvas.style.border = `solid 2px black`;
 body.appendChild(canvas);
+
+function loadCell () {
+    const cell = makeDiv;
+    const cellSize = (canvasSizeNum / rowAmount) + `px`;
+    cell.style.width = cellSize;
+    cell.style.height = cellSize;
+    cell.style.backgroundColor = `white`;
+    canvas.appendChild(cell); 
+};
 
