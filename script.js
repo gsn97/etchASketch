e4ff308714bab85e54f5276488651ae19e1381ce
gsn97 <186332589+gsn97@users.noreply.github.com>
2025-11-10 19:23:56 -0500

    Create empty canvas

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const body = document.querySelector(`body`);
 body.style.backgroundColor = `tan`;
+
+const canvasSizeNum = 500;
+const canvasSize = canvasSizeNum + `px`;
+
+const canvas = document.createElement(`div`);
+canvas.style.width = canvasSize;
+canvas.style.height = canvasSize;
+canvas.style.border = `solid 2px black`;
+body.appendChild(canvas);
+
